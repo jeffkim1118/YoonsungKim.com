421749f27d5d49cb90ee676b5d640a4b938141d9
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-27 13:15:00 -0400

    Contact form added
    Added contact form to let visitors to send me messages.

---
+++ src/components/contact.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Warning from "./warning";
+
+function Contact(){
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[message, setMessage] = useState("");
+    const[status, setStatus] = useState(true);
+
+    const form = useRef();
+
+    function handleSubmit(e){
+        e.preventDefault();
+        if(name === "" || email === "" || message === ""){
+            setStatus(false);
+        }else{
+            emailjs.sendForm('service_qufbpuo', 'template_4y8qqhm', form.current, 'BYtwJwTOulJN4yq_L')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+                console.log("message sent");
+            }, (error) => {
+            console.log(error.text);
+            });
+        }
+    }
+
+    return(
+        <div className="email-form-container">
+            <h1>Contact Me</h1>
+            {!status ? <Warning /> : null}
+            <form className="contact-form" ref={form} onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="user_name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <label>Email</label>
+                <input type="email" name="user_email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <label>Message</label>
+                <textarea name="message" value={message} onChange={(e)=>setMessage(e.target.value)}/>
+                <input type="submit" value="Send" />
+            </form>
+        </div>
+    )
+}
+export default Contact;
