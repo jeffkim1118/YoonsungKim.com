ad73ce81142e47bdeb380b1f8606789e49129c98
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-30 21:33:11 -0400

    changed background-position to center bottom

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,24 +3,33 @@ import emailjs from '@emailjs/browser';
 import Warning from "./warning";
 
 function Contact(){
-    const[name, setName] = useState();
-    const[email, setEmail] = useState();
-    const[msg, setMsg] = useState();
-    
-    function handleSubmit(){
+    const[formStatus, setStatus] = useState(Boolean);
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[msg, setMsg] = useState("");
 
+    function handleSubmit(e: any){
+        e.preventDefault();
+       
     }
 
     return(
-        <div>
-            <form onSubmit={() => {handleSubmit()}}>
+        <div className="contact-form-container">
+            {/* {formStatus === false ? <Warning /> : null} */}
+            <form className="contact-form" onSubmit={(e: React.SyntheticEvent) => {handleSubmit(e)}}>
+                <div className="form-group">
+
                 <label>Name</label>
-                <input type="text"></input>
+                <input className="form-control" type="text" placeholder="Your name" value={name} onChange={(e) => setName(e.target.value)} ></input>
+                
                 <label>Email</label>
-                <input type="email"></input>
+                <input className="form-control" type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                
                 <label>Your Message</label>
-                <input type="text"></input>
-                <button type="submit">Send</button>
+                <textarea className="form-control" rows={parseFloat("3")} placeholder="Type something..." value={msg} onChange={(e) => setMsg(e.target.value)}></textarea>
+
+                <button type="submit" className="btn btn-success">Send</button>
+                </div>     
             </form>
         </div>
     )
