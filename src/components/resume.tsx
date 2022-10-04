319ef9302a4276ee5dbf9578a437e8bc5930dbf3
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-03 21:51:34 -0400

    Added resume section

---
+++ src/components/resume.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Pdf from "../images/resume.pdf";
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+export default function Resume(){
+    
+    return(
+        <div className="resume-section">
+            <h2>Resume</h2>
+            <div className="pdf-container">      
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+                    <Viewer fileUrl={Pdf} />  
+                </Worker>
+                <a href={Pdf} type="button" className="btn btn-primary" target="_blank"  rel="noreferrer">View Resume</a>              
+            </div>
+        </div>
+    )
+}
