343de7c1fc0cdd70ec35a913c699baf9385dd736
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-14 21:36:34 -0400

    Moved hamburger bar to the left side

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,6 @@ function Navbar(){
       <div className="navbar-container">
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#/">Yoonsung Kim</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
